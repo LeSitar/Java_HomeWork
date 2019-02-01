b77846978467fbbafa8c9dbd19a2da35e3e59a99
LeSitar <lenasitars@gmail.com>
2019-02-01 11:26:43 +0100

    first solution

---
+++ 2019_02_01_ReturnAnagramm/src/Main.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String n1 = "Ivan";
+		String n2 = "Anna";
+		
+		System.out.println(isWordAnagramm(n1, n2));
+		
+		List <String> words = new ArrayList<>();
+		Collections.addAll(words, "Ivan", "Oleg", "Anna", "Nana", "Navi", 
+				"Inav","Max","Robert");
+		System.out.println(returnAnagramms(words, "Anna"));
+
+	}
+	
+	
+	public static List<String> returnAnagramms(List<String> words, String word){
+		List<String> anagramms = new ArrayList<>();
+		//anagramms.add(word);
+		
+		for (String w : words) {
+			if(isWordAnagramm(w, word)) {
+				anagramms.add(w);
+
+			}
+		}
+		
+		return anagramms;
+
+	}
+	
+	public static boolean isWordAnagramm(String a, String b ) {
+		
+		if (a.length()==b.length()) {
+			
+			a = a.toLowerCase();
+			b = b.toLowerCase();
+			
+			List<Character> charListA = new ArrayList<Character>();
+
+			for (char c : a.toCharArray()) {
+			  charListA.add(c);
+			}
+
+			List<Character> charListB = new ArrayList<Character>();
+
+			for (char c : b.toCharArray()) {
+			  charListB.add(c);
+			}
+			
+			return charListA.containsAll(charListB);
+			
+			
+			
+		}
+		
+		
+		return false;
+	}
+
+}
